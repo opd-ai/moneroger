0fd4d16ec1b08f362e2fd632d79dac8e4b595645
user <user@mail.com>
2025-01-13 11:44:39 -0500

    add remote node support to the wallet-rpc side.

--- monero-wallet-rpc/util.go
+++ monero-wallet-rpc/util.go
@@ -1,7 +1,12 @@
 package monerowalletrpc
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+)
 
+// validateRemoteDaemon ensures that the remote daemon URL is correctly formed
+// and outputs the components of the URL.
 func validateRemoteDaemon(uri string) (scheme, host, port string, err error) {
 	newUri, err := url.Parse(uri)
 	if err != nil {
@@ -11,6 +16,10 @@ func validateRemoteDaemon(uri string) (scheme, host, port string, err error) {
 	port = newUri.Port()
 	if port == "" {
 		port = "18081"
+	}
+	if len(newUri.Path) > 1 {
+		err = fmt.Errorf("Remote node URLs may not contain a path: %s", newUri.Path)
+		return
 	}
 	return
 }
